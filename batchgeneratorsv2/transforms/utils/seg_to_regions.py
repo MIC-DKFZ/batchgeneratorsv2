155cb61f2d5057c8386f9f9be4833cd0721e50dd
Fabian Isensee <f.isensee@dkfz.de>
2024-04-25 14:09:08 +0200

    speed improvement for ConvertSegmentationToRegionsTransform

--- batchgeneratorsv2/transforms/utils/seg_to_regions.py
+++ batchgeneratorsv2/transforms/utils/seg_to_regions.py
@@ -13,10 +13,10 @@ class ConvertSegmentationToRegionsTransform(SegOnlyTransform):
 
     def _apply_to_segmentation(self, segmentation: torch.Tensor, **params) -> torch.Tensor:
         num_regions = len(self.regions)
-        region_output = torch.zeros((num_regions, *segmentation.shape[1:]), dtype=segmentation.dtype, device=segmentation.device)
+        region_output = torch.zeros((num_regions, *segmentation.shape[1:]), dtype=torch.bool, device=segmentation.device)
         for region_id, region_source_labels in enumerate(self.regions):
             if not isinstance(region_source_labels, (list, tuple)):
                 region_source_labels = (region_source_labels, )
             for label_value in region_source_labels:
-                region_output[region_id][segmentation[self.channel_in_seg] == label_value] = 1
-        return region_output
+                region_output[region_id] |= (segmentation[self.channel_in_seg] == label_value)
+        return region_output.to(segmentation.dtype)
